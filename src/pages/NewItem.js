861c9af7e651743f32d52142bef1cd75866b2edc
yosh1 <yoppy1125yoppy@gmail.com>
2021-07-07 15:40:05 +0900

    Add new pages

---
+++ src/pages/NewItem.js
@@ -0,0 +1,28 @@
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import React from 'react';
+
+const NewItem = () => {
+  return(
+     <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonButtons slot="start">
+          <IonBackButton defaultHref="/home" />
+        </IonButtons>
+        <IonTitle>New Item</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent />
+  </IonPage>
+  );
+};
+
+export default NewItem;
